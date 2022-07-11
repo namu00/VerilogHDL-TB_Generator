b582531cc4adf4077fac6322b1d498bc933068e6
namu00 <skagns3571@naver.com>
2022-07-11 15:21:13 +0900

    Python Study

--- Python_Study/Unit 29 함수/practice_function.py
+++ Python_Study/Unit 29 함수/practice_function.py
@@ -14,9 +14,10 @@
 #몫 : 3, 나머지 : 1
 
 #####################################################
-
+x = 10; y = 3
 def get_quotient_remainder(a,b):
     return a//b, a%b
+
 
 quotient, remainder = get_quotient_remainder(x, y)
 print('몫: {0}, 나머지: {1}'.format(quotient, remainder))
