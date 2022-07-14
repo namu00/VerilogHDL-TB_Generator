d454402b7dfee99cd9e31465dbd928591758edfe
namu00 <skagns3571@naver.com>
2022-07-14 14:56:30 +0900

    Verilog Study

--- Verilog_Study/Verilog_Codes/Combinational_Logic/09_parity_checker/src/rtl/parity_check.v
+++ Verilog_Study/Verilog_Codes/Combinational_Logic/09_parity_checker/src/rtl/parity_check.v
@@ -12,11 +12,9 @@ module parity_checker(data, parity, trig, out);
         if (trig == 1'b0) begin
             out = (data[3] ^ data[2] ^ data[1] ^ data[0] ^ parity);
         end
-        else if(trig == 1'b1) begin
-            out  = ~(data[3] ^ data[2] ^ data[1] ^ data[0] ^ parity);
-        end
+
         else begin
-            out = 1'bz;
+            out  = ~(data[3] ^ data[2] ^ data[1] ^ data[0] ^ parity);
         end
 
     end
