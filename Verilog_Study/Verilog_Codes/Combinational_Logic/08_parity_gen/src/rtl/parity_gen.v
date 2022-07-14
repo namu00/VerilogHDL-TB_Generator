1af54489fe3986ea847e834fd43e8adb10967752
namu00 <skagns3571@naver.com>
2022-07-14 10:40:04 +0900

    Verilog Study

--- Verilog_Study/Verilog_Codes/Combinational_Logic/08_parity_gen/src/rtl/parity_gen.v
+++ Verilog_Study/Verilog_Codes/Combinational_Logic/08_parity_gen/src/rtl/parity_gen.v
@@ -1,12 +1,20 @@
 module parity_gen (data, trig, parity);
     input [3:0] data;
     input trig;
-    output parity;
+    output reg parity;
 
     //trig == 0 / odd_parity
     //trig == 1 / even_parity
 
-    assign parity = (trig == 0) ? 
-        (data[0] ^ data[1] ^ data[2] ^ data[3]) :
-        ~(data[0] ^ data[1] ^ data[2] ^ data[3]);
+    always @ (trig or data)begin
+        if (trig == 1'b0) begin
+            parity = ~(data[3] ^ data[2] ^ data[1] ^ data[0]);
+        end
+        else if(trig == 1'b1) begin
+            parity  = (data[3] ^ data[2] ^ data[1] ^ data[0]);
+        end
+        else begin
+            parity = 1'bz;
+        end
+    end
 endmodule 
--- Verilog_Study/Verilog_Codes/Combinational_Logic/08_parity_gen/testbench/testbench.v
+++ Verilog_Study/Verilog_Codes/Combinational_Logic/08_parity_gen/testbench/testbench.v
@@ -2,11 +2,26 @@ module testbench();
     reg [3:0] data;
     reg trig;
 
-    wire odd_parity;
-    wire even_parity;
+    wire parity;
 
-    parity_gen odd(
+    parity_gen unit_0(
         .data(data),
-        .trig(data),
-        .parity(odd_parity)
+        .trig(trig),
+        .parity(parity)
     );
+
+    integer i;
+
+    initial begin
+      trig = 1'b0;
+      for (i = 4'b0; i <= 4'b1111; i = i + 1'b1)begin
+        data = i; #5;
+      end
+
+      trig = 1'b1;
+      for (i = 4'b0; i <= 4'b1111; i = i + 1'b1)begin
+        data = i; #5; 
+      end
+      $stop;
+    end
+endmodule
