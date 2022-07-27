5dfb6a6dd1eb6e2de2b83a2e2ae5c37fcad77eee
skagns <skagns3571@naver.com>
2022-07-27 11:15:27 +0900

    Verilog Study

---
+++ Verilog_Study/Verilog_Codes/Sequential_Logic/01_SR_Latch/src/rtl/sr_latch.v
@@ -0,0 +1,18 @@
+module sr_latch(s,r,out);
+    input s;
+    input r;
+    output reg out;
+
+    assign out = 1'b0;
+
+    always@(s,r)begin
+        if (s == 1'b1 && r == 1'b0 ) 
+            out = 1'b1;
+        else if (r == 1'b1 && s == 1'b0) 
+            out = 1'b0;
+        else if (r == 1'b0 && s == 1'b0)
+            out = out;
+        else 
+            out = 1'bx; //forbidden
+    end
+endmodule
