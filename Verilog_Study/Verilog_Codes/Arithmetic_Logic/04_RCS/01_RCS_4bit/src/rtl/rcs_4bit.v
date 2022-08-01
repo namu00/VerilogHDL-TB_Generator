fd9f14726bd07ea64e892a5fca1e57531eac4dd1
skagns <skagns3571@naver.com>
2022-08-01 10:27:56 +0900

    Verilog Study

--- Verilog_Study/Verilog_Codes/Arithmetic_Logic/04_RCS/01_RCS_4bit/src/rtl/rcs_4bit.v
+++ Verilog_Study/Verilog_Codes/Arithmetic_Logic/04_RCS/01_RCS_4bit/src/rtl/rcs_4bit.v
@@ -1,56 +1,64 @@
-module rcs_4bit(a, b, borrow_in, borrow,diff);
-    input [3:0] a,b;
-    input borrow_in;
-    output [3:0] diff;
-    output borrow;
+//Ripple Carry Subtractor
+//Using Full-Adder Blocks
 
-    wire [3:0] brr;
+//One of Operand have to be set as Two's Compliment.
+//I will make B bits as Two's Compliment.
+module rcs_4bit(a, b, sum, carry_start, carry);
+    input [3:0] a;
+    input [3:0] b;
+    input carry_start;
 
-    full_subtract unit_0(
+    output [3:0] sum;
+    output carry;
+
+    wire [2:0] c;
+
+    full_adder unit_0( //LSB
         .a(a[0]),
-        .b(b[0]),
-        .c(borrow_in),
-        .diff(diff[0]),
-        .borrow(brr[0])
+        .b(~b[0]), //Bit Inverting
+        .c_in(1'b1), // For Two's Compliment. (It means, ~b + 1'b1)
+        .sum(sum[0]),
+        .c_out(c[0])
     );
 
-    full_subtract unit_1(
+    full_adder unit_1(
         .a(a[1]),
-        .b(b[1]),
-        .c(brr[0]),
-        .diff(diff[1]),
-        .borrow(brr[1])
+        .b(~b[1]), //Bit Inverting
+        .c_in(c[0]),
+        .sum(sum[1]),
+        .c_out(c[1])
     );
 
-    full_subtract unit_2(
+    full_adder unit_2(
         .a(a[2]),
-        .b(b[2]),
-        .c(brr[1]),
-        .diff(diff[2]),
-        .borrow(brr[2])
+        .b(~b[2]), //Bit Inverting
+        .c_in(c[1]),
+        .sum(sum[2]),
+        .c_out(c[2])
     );
 
-    full_subtract unit_3(
+    full_adder unit_3( //MSB
         .a(a[3]),
-        .b(b[3]),
-        .c(brr[2]),
-        .diff(diff[3]),
-        .borrow(brr[3])
+        .b(~b[3]), //Bit Inverting
+        .c_in(c[2]),
+        .sum(sum[3]),
+        .c_out(carry)
     );
-
-    assign borrow = brr[3];
 endmodule
 
-module full_subtract(a,b,c,borrow,diff);
-    input a,b,c;
-    output borrow,diff;
+module full_adder(a, b, c_in, sum, c_out);
+    input a, b, c_in;
+    output sum, c_out;
+    wire s1;
+    wire c1, c2, c3;
 
-    wire [3:0]w;
-    xor (diff,a,b,c);
+    //Carry_OUT
+    and carry0 (c1, a, b); //A AND B
+    xor carry1 (c2, a, b); 
+    and carry2 (c3, c2, c_in); // (A ^ B) AND C_in
+    or carry3 (c_out, c1, c3); // (A&B) + (A ^ B)&C_in
 
-    not n1(w[0],a);
-    and a1(w[1],w[0],b);
-    and a2(w[2],w[1],c);
-    and a3(w[3],b,c);
-    or o1(borrow,w[1],w[2],w[3]);
+    //SUM
+    xor sum1 (s1, a, b);
+    xor sum2 (sum, s1, c_in);
 endmodule
