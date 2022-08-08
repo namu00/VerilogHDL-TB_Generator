2d01574d93dd3288b9c83eb063b2f2f26895cc91
skagns <skagns3571@naver.com>
2022-08-08 16:02:51 +0900

    Verilog Study

--- Verilog_Study/Verilog_Codes/Sequential_Logic/03_Counter/03_Johnson_Counter/src/rtl/jcnt.v
+++ Verilog_Study/Verilog_Codes/Sequential_Logic/03_Counter/03_Johnson_Counter/src/rtl/jcnt.v
@@ -1,47 +1,14 @@
-module dff(clk, rst, d, out);
-    input clk;
-    input rst;
-    input d;
-    output reg out;
-
-    always @ (posedge clk, negedge rst)begin
-        if(!rst) out <= 1'b0;
-        else if (d == 1'b1) out <= 1'b1;
-        else out <= 1'b0;
-    end
-endmodule
-
 module jcnt(clk, rst, q);
     input clk;
     input rst;
     output [3:0] q;
 
-    dff unit_3(
-        .clk(clk),
-        .rst(rst),
-        .d(~q[0]),
-        .out(q[3])
-    );
+    reg [3:0]d;
 
-    dff unit_2(
-        .clk(clk),
-        .rst(rst),
-        .d(q[3]),
-        .out(q[2])
-    );
-    
-    dff unit_1(
-        .clk(clk),
-        .rst(rst),
-        .d(q[2]),
-        .out(q[1])
-    );
-    
-    dff unit_0(
-        .clk(clk),
-        .rst(rst),
-        .d(q[1]),
-        .out(q[0])
-    );
-    
+    always@(posedge clk, negedge rst)begin
+        if(!rst)  d <= 4'b0000;
+        else {d[3:1], d[0]} <= {d[2:0],~d[3]};
+    end
+
+    assign q = d;
 endmodule
